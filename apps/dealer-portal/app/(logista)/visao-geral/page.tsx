04bec6ffbdf640224f5085a0bb7aa2dc7bd827a4
GrotaFinanciamentos <devops@grotafinanciamentos.com.br>
2025-11-21 10:11:56 +0000

    fix

--- apps/dealer-portal/app/(logista)/visao-geral/page.tsx
+++ apps/dealer-portal/app/(logista)/visao-geral/page.tsx
@@ -453,7 +453,10 @@ export default function Page() {
         </div>
       </section>
 
-      <RealtimeBridgePanel />
+      {/*
+       <RealtimeBridgePanel />
+      */}
+     
 
       <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-4">
         {kpiCards.length
